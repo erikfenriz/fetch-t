290af19660dc28ac552f61ab8081763a11a89c8f
Erik Rybalkin <erikfenriz@gmail.com>
2018-04-28 12:41:24 +0300

    random profiles done

--- script/fetch.js
+++ script/fetch.js
@@ -1,22 +1,31 @@
-let btn = document.getElementById("btn");
-let img = document.getElementById("avatar");
+let d = document;
+let btn = d.getElementById("btn");
+let img = d.getElementById("avatar");
+let email = d.getElementById("email");
+let id = d.getElementById("username");
+let name = d.getElementById("fullname");
+let address = d.getElementById("city");
 btn.addEventListener("click", function(){
     let url = "https://randomuser.me/api/";
     fetch(url)
         .then(handleErrors)
         .then(function(response){
             console.log("Steady!");
-            console.log(response);
             return response.json(response).then(function (data) {
-                console.log(data);
 
+                let user = data.results[0];
+                console.log(user);
+                img.src = user.picture.large;
+                name.innerHTML = user.name.first.toUpperCase() + " " + user.name.last.toUpperCase();
+                id.innerHTML = user.login.username;
+                email.innerHTML = user.email;
+                address.innerHTML = user.location.city.charAt(0).toUpperCase()  + user.location.city.slice(1);;
             })
         })
         .catch(function(error){
             console.log(error);
         });
 });
-
 function handleErrors (request){
     if(!request.ok) {
         throw Error(request.status);
